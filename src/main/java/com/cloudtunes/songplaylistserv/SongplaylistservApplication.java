6f870e0c6735af9754a0e7518fdf4fa162438de5
ashsnep <marcdominic26@gmail.com>
2024-10-16 11:11:22 +0200

    Added base service

---
+++ src/main/java/com/cloudtunes/songplaylistserv/SongplaylistservApplication.java
@@ -0,0 +1,13 @@
+package com.cloudtunes.songplaylistserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongplaylistservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongplaylistservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudtunes/songplaylistserv/SongplaylistservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudtunes.songplaylistserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongplaylistservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
